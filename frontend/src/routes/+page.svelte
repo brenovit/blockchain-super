c7ad470ba37014c3a6e154bcd56d7fd2fa9cdbf2
Breno Arantes <breno.arantes97@gmail.com>
2025-03-09 19:43:17 +0100

    Create frontend

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div class="container py-4 px-3 mx-auto">
+	<h1>Hello, Bootstrap and Vite!</h1>
+	<h1>Welcome to SvelteKit</h1>
+	<button class="btn btn-primary">Primary button</button>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+</div>
