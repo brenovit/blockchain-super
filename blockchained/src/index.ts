fbad19fb05038897b136b1fcb2fc8154ba0e6aa6
Breno Arantes <breno.arantes97@gmail.com>
2025-03-09 22:44:37 +0100

    Create flow to update and mine blocks
    - Return chain status with error messages
    - Reset nounce and hash when mining block

--- blockchained/src/index.ts
+++ blockchained/src/index.ts
@@ -33,16 +33,19 @@ class Block {
   }
 
   mine(difficult: number): void {
+    this.nonce = 0;
+    this.hash = this.calculateHash();
     while (
       this.hash.substring(0, difficult) != Array(difficult + 1).join("0")
     ) {
       this.nonce++;
       this.hash = this.calculateHash();
     }
+    this.checkValid();
   }
 
-  get isValid() {
-    return;
+  checkValid() {
+    this.valid = this.hash == this.calculateHash();
   }
 }
 
@@ -64,40 +67,87 @@ class Blockchain {
   }
 
   createAndBlock(data: any): void {
+    console.log("Creating block to add in the chain");
     const newBlock = new Block(data);
     newBlock.index = this._chain.length;
     newBlock.previousHash = this.getLatestBlock().hash;
     newBlock.mine(this.difficulty);
     this._chain.push(newBlock);
+    console.log("Block created and added");
+
+    console.log("<=><=><=><=><=><=><=><=><=><=>");
+    console.log(this._chain);
+    console.log("<=><=><=><=><=><=><=><=><=><=>");
   }
 
   addBlock(newBlock: Block): void {
+    console.log("Adding block to the chain");
     newBlock.index = this._chain.length;
     newBlock.previousHash = this.getLatestBlock().hash;
     newBlock.mine(this.difficulty);
     this._chain.push(newBlock);
+    console.log("Block added");
+
+    console.log("<=><=><=><=><=><=><=><=><=><=>");
+    console.log(this._chain);
+    console.log("<=><=><=><=><=><=><=><=><=><=>");
   }
 
-  isChainValid() {
+  mineBlock(index: number) {
+    console.log("Mining block: " + index);
+    const block = this._chain[index];
+    block.mine(this.difficulty);
+    console.log("Block mined");
+
+    console.log("<=><=><=><=><=><=><=><=><=><=>");
+    console.log(this._chain);
+    console.log("<=><=><=><=><=><=><=><=><=><=>");
+  }
+
+  updateBlock(payload: any) {
+    console.log("Updating block: " + payload.index);
+    const block = this._chain[payload.index];
+    block.nonce = payload.nonce;
+    block.data = payload.data;
+    block.previousHash = payload.previousHash;
+    block.checkValid();
+    console.log("Block updated");
+
+    console.log("<=><=><=><=><=><=><=><=><=><=>");
+    console.log(this._chain);
+    console.log("<=><=><=><=><=><=><=><=><=><=>");
+  }
+
+  getChainStatus() {
+    const errors: string[] = [];
     for (let i = 0; i < this._chain.length; i++) {
       const currentBlock = this._chain[i];
       const previousBlock = this._chain[i - 1];
 
-      if (currentBlock.hash != currentBlock.calculateHash()) {
-        return false;
+      if (!currentBlock.valid) {
+        errors.push(
+          `The block #${
+            currentBlock.index
+          } is invalid. Calculated hash: ${currentBlock.calculateHash()}`
+        );
       }
 
       if (previousBlock && currentBlock.previousHash != previousBlock.hash) {
-        return false;
+        errors.push(
+          `The block #${currentBlock.index} is pointing to an inexistent block. The hash does not match with previous block #${previousBlock.index}`
+        );
       }
     }
-    return true;
+    return {
+      valid: errors.length == 0,
+      errors: errors,
+    };
   }
 
-  get chain() {
+  get data() {
     return {
-      blocks: this._chain,
-      valid: this.isChainValid(),
+      chain: this._chain,
+      status: this.getChainStatus(),
     };
   }
 
--- blockchained/src/server.ts
+++ blockchained/src/server.ts
@@ -10,10 +10,10 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/blockchain", (req, res) => {
-  res.json(blockchain.chain);
+  res.json(blockchain.data);
 });
 
-app.post("/add-block", (req: any, res: any) => {
+app.post("/create-block", (req: any, res: any) => {
   const { data } = req.body;
   if (!data) {
     return res.status(400).json({ error: "Data is required" });
@@ -21,6 +21,26 @@ app.post("/add-block", (req: any, res: any) => {
 
   const newBlock = blockchain.createAndBlock(data);
   res.json(newBlock);
+});
+
+app.post("/mine-block/:index", (req: any, res: any) => {
+  const index = req.params["index"];
+  if (!index) {
+    return res.status(400).json({ error: "index is required" });
+  }
+
+  const minedBlock = blockchain.mineBlock(index);
+  res.json(minedBlock);
+});
+
+app.post("/update-block", (req: any, res: any) => {
+  const { data } = req.body;
+  if (!data) {
+    return res.status(400).json({ error: "Data is required" });
+  }
+
+  const minedBlock = blockchain.updateBlock(data);
+  res.json(minedBlock);
 });
 
 const PORT = 3001;
